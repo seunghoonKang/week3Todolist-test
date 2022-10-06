e0f5abf5e92b8808a14118172dd7325cab03866b
seunghoon <tmdgns4321@gmail.com>
2022-10-06 11:26:19 +0900

    3주차 테스트

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [contents, setContents] = useState([
+    {
+      id: 1,
+      content: "테스트",
+    },
+    {
+      id: 2,
+      content: "테스트 2",
+    },
+  ]);
+  const nextId = useRef(3);
+
+  const onCreate = (payload) => {
+    console.log(payload);
+    setContents([
+      ...contents,
+      {
+        id: (nextId.current += 1),
+        content: payload,
+      },
+    ]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form onCreate={onCreate} />
+      <List contents={contents} />
+    </div>
+  );
+}
+
+//폼 (인풋, 추가하기)
+function Form({ onCreate }) {
+  const [content, setContent] = useState("");
+
+  const onChangeHandler = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(content);
+    onCreate(content);
+    setContent("");
+  };
+
+  return (
+    <form onSubmit={onSubmitHandler}>
+      <input onChange={onChangeHandler} value={content}></input>
+      <button type="submit">추가하기</button>
+    </form>
+  );
+}
+
+function List({ contents }) {
+  console.log(contents);
+  return (
+    <div>
+      <h1 className="todoListName">Todo List</h1>
+      <div className="lists">
+        {contents.map((item) => {
+          return (
+            <div>
+              <div className="listContent" key={item.id}>
+                {item.content}
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
